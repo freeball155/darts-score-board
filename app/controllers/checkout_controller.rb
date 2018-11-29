00291faf5b9270205cff111b778ec8d5beb85443
Sergey Budnitsky <serbud@gmail.com>
2018-11-29 10:37:12 +0200

    highlight green possible checkout according with darts remaining

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,10 +1,14 @@
 class CheckoutController < ApplicationController
   def show
-    if checkout = Checkout.find_by(points: params[:id])
+    if checkout = Checkout.find_by(points: params[:points])
       num_darts = checkout.combination.split(' ').length
-      render json: {"points": params[:id].to_i, "combination": checkout.combination, num_darts: num_darts}
+      if num_darts <= params[:num_darts].to_i
+        render json: {"points": params[:points].to_i, "combination": checkout.combination, num_darts: num_darts}
+      else
+        render json: {"points": params[:points].to_i, "combination": "NA", num_darts: 0}
+      end
     else
-      render json: {"points": params[:id].to_i, "combination": "NA", num_darts: 0}
+      render json: {"points": params[:points].to_i, "combination": "NA", num_darts: 0}
     end
   end
 end
